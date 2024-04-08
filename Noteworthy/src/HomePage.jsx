78c0e1a6d0b4028ace366d7077ec017c04fba097
Sophia <sofaz2214@gmail.com>
2024-04-08 13:27:11 -0700

    updates on app and homepage

---
+++ Noteworthy/src/HomePage.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import noteworthyImage from './noteworthy.jpg'; 
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <div className="cover"></div> 
+                <div className="title">
+                    <h1>Noteworthy Acapella</h1>
+                </div>
+                <div className="line"></div>
+        </div>
+    )
+}
+
+export default HomePage;
